d76602b2dd76daa641436999008248d1e481d017
acecle <>
2020-03-23 22:22:06 +0000

    Updates

--- precache-manifest.ca1a838e1bb66099f2d693e6ba6b3fa0.js
+++ precache-manifest.ca1a838e1bb66099f2d693e6ba6b3fa0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e490baf4b61e8909c66f3c4ef9678841",
+    "revision": "e45c654f771152462929adedd5aa6650",
     "url": "/facerec-client/index.html"
   },
   {
-    "revision": "7173f25889b0d5d4bb3f",
+    "revision": "c0b19c47765f15968975",
     "url": "/facerec-client/static/css/main.76e27cc9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/facerec-client/static/js/2.f4042acc.chunk.js.LICENSE"
   },
   {
-    "revision": "7173f25889b0d5d4bb3f",
-    "url": "/facerec-client/static/js/main.15b253ee.chunk.js"
+    "revision": "c0b19c47765f15968975",
+    "url": "/facerec-client/static/js/main.89c09ceb.chunk.js"
   },
   {
     "revision": "27c003f7423061b01c68",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facerec-client/precache-manifest.070fa72263b50b560a58691f3e168c02.js"
+  "/facerec-client/precache-manifest.ca1a838e1bb66099f2d693e6ba6b3fa0.js"
 );
 
 self.addEventListener('message', (event) => {
